f20191842095ec94cd1bdc31c63205c414c4ddaf
Russell Huebner <russell.j.huebner@outlook.com>
2020-04-23 10:31:54 -0700

    Add project files.

---
+++ ChuckNorrisIO.UnitTest/ChuckNorrisClientUnitTest.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChuckNorrisIO.UnitTest
+{
+    [TestClass]
+    public class ChuckNorrisClientUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
